d7e8a8ec8cb041b7e9b6fdd073852494103237b5
Mirek Komárek <mkomarek@gmail.com>
2024-10-07 14:08:01 +0200

    Update Breadcrumb.vue

--- components/dashboard/Breadcrumb.vue
+++ components/dashboard/Breadcrumb.vue
@@ -14,7 +14,7 @@ defineProps({
     <BreadcrumbList>
       <BreadcrumbItem>
         <BreadcrumbLink href="/">
-          Sink
+          CPI Links
         </BreadcrumbLink>
       </BreadcrumbItem>
       <BreadcrumbSeparator />
